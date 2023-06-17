091dcef347961f72827206c429c3e9b205b50669
DayanaGamboaM <mongeg1715@gmail.com>
2023-06-17 17:20:37 -0600

    consume ChatGPT Api

--- examenll/components/ChatRecipes.tsx
+++ examenll/components/ChatRecipes.tsx
@@ -1,25 +1,61 @@
 import React, { useState } from 'react';
 
 const ChatRecipes: React.FC = () => {
-  const [question, setQuestion] = useState('');
+  const [inputParams, setInputParams] = useState('');
   const [response, setResponse] = useState('');
+  const apiKey = "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";
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuestion(event.target.value);
+    setInputParams(event.target.value);
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (!question) {
-      setResponse('Por favor, ingresa los ingredientes.');
+    if (!inputParams) {
+      setResponse('Por favor, ingresa los parámetros.');
       return;
     }
 
+    const [ingredientes, tipo, dificultad, cantidadSalidas] = inputParams.split(';');
+
     setResponse('Haciendo la solicitud al API...');
 
-    
-    setQuestion('');
+    try {
+      const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          Authorization: 'Bearer ' + apiKey,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          model: 'gpt-3.5-turbo',
+          messages: [
+            { role: 'system', content: 'You are a helpful assistant.' },
+            { role: 'user', content: "Generar recetas;${ingredientes};${tipo};${dificultad};${cantidadSalidas} "},
+          ],
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data && data.choices && data.choices.length > 0) {
+        const message = data.choices[0].message;
+
+        if (message && message.content) {
+          setResponse(message.content);
+        } else {
+          setResponse('Error al obtener la respuesta del API');
+        }
+      } else {
+        setResponse('Error al obtener la respuesta del APIIII');
+      }
+    } catch (error) {
+      console.error('Error al hacer la solicitud al API:', error);
+      setResponse('Error al hacer la solicitud al API');
+    }
+
+    setInputParams('');
   };
 
   return (
@@ -32,8 +68,8 @@ const ChatRecipes: React.FC = () => {
               <input
                 type="text"
                 className="form-control"
-                placeholder="Escribe tu ingredientes aquí"
-                value={question}
+                placeholder="Escribe los parámetros separados por ; (por ejemplo: ingredientes;tipo;dificultad;cantidadSalidas)"
+                value={inputParams}
                 onChange={handleInputChange}
               />
               <button className="btn btn-primary" type="submit">
--- examenll/src/pages/index.tsx
+++ examenll/src/pages/index.tsx
@@ -4,12 +4,10 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '../../components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import SectionAbout from '../../components/SectionAbout'
 import Footer from '../../components/Footer'
-import NavBar from '../../components/NavBar'
-
-
 import ChatRecipes from '../../components/ChatRecipes'
 const Index: React.FC = () => {
   return (
@@ -18,7 +16,6 @@ const Index: React.FC = () => {
         <title>ChatGPT Barato</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar />
       <Header />
       <SectionAbout />
       <ChatRecipes />
