07c464956e69ede72de8d46287f3a034eb0718bd
Maria-Chacon <123408968+Maria-Chacon@users.noreply.github.com>
2023-06-17 13:03:47 -0600

    NavBar creation

---
+++ examenll/components/NavBar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import Image from 'next/image';
+import logo from '../public/dm.jpg';
+
+
+const NavBar: React.FC = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">
+      <Image src={logo} alt="Mi logo" width="75" height="75" className="img-fluid rounded-circle" style={{marginLeft:'20px'}} />
+        
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Inicio</Nav.Link>
+          <Nav.Link href="#about">Acerca de</Nav.Link>
+          <Nav.Link href="#chat">Chat</Nav.Link> 
+          <Nav.Link href="#contact">Contacto</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
--- examenll/src/pages/index.tsx
+++ examenll/src/pages/index.tsx
@@ -4,10 +4,12 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '../../components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import SectionAbout from '../../components/SectionAbout'
 import Footer from '../../components/Footer'
+import NavBar from '../../components/NavBar'
+
+
 const Index: React.FC = () => {
   return (
     <>
@@ -15,6 +17,7 @@ const Index: React.FC = () => {
         <title>ChatGPT Barato</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <NavBar />
       <Header />
       <SectionAbout />
       <Footer />
